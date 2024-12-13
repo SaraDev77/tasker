a57e65b9ae625871a73c4c7b178983b4335234d9
sara <sara.yousry@brightskiesinc.com>
2024-12-13 14:54:59 +0200

    fix: msg adjustment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { Message } from 'primevue'
 import HomeComponent from '../components/HomeComponent.vue'
 import KnobComponent from '../components/KnobComponent.vue'
+
+const showWarning = ref(false)
+
+onMounted(() => {
+  showWarning.value = true
+  setTimeout(() => {
+    showWarning.value = false
+  }, 60000)
+})
 </script>
 
 <template>
   <KnobComponent />
   <div class="max-w-full mx-2">
-    <Message severity="warn" closable
-      >If You Want To Delete Overdue Tasks (Red Colored Cards)You Should Contact Your Super
+    <Message v-if="showWarning" severity="warn" closable
+      >If You Want To Delete Overdue Tasks (Red Colored Cards) You Should Contact Your Super
       Admin</Message
     >
   </div>
