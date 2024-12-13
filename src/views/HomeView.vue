954f2fab9d6692d5b90e19b62cc5b5fa569dc01f
sara <sara.yousry@brightskiesinc.com>
2024-12-13 14:50:34 +0200

    feat: add warning msg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { Message } from 'primevue'
 import HomeComponent from '../components/HomeComponent.vue'
-import KnobComponent from '../components/KnobComponent.vue';
+import KnobComponent from '../components/KnobComponent.vue'
 </script>
 
 <template>
-<KnobComponent/>
-<HomeComponent />
+  <KnobComponent />
+  <div class="max-w-full mx-2">
+    <Message severity="warn" closable
+      >If You Want To Delete Overdue Tasks (Red Colored Cards)You Should Contact Your Super
+      Admin</Message
+    >
+  </div>
+  <HomeComponent />
 </template>
