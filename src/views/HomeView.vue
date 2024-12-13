572cbdb7bb3115f5bbe0ffccc81f71c9e1ef45ec
sara <sara.yousry@brightskiesinc.com>
2024-12-13 15:02:01 +0200

    fix: msg edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,11 @@ import { ref, onMounted } from 'vue'
 import { Message } from 'primevue'
 import HomeComponent from '../components/HomeComponent.vue'
 import KnobComponent from '../components/KnobComponent.vue'
+import { useAuthStore } from '../stores/auth.store'
+import { UserRole } from '../models/user-role.enum'
 
 const showWarning = ref(false)
+const authStore = useAuthStore()
 
 onMounted(() => {
   showWarning.value = true
@@ -17,7 +20,7 @@ onMounted(() => {
 <template>
   <KnobComponent />
   <div class="max-w-full mx-2">
-    <Message v-if="showWarning" severity="warn" closable
+    <Message v-if="showWarning && authStore.user?.role === UserRole.ADMIN" severity="warn" closable
       >If You Want To Delete Overdue Tasks (Red Colored Cards) You Should Contact Your Super
       Admin</Message
     >
